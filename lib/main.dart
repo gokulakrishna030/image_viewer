0fb26bbbaccd355fe5aca9a7d051e289f501569d
gokulakrishna030 <smartgokul030@gmail.com>
2023-06-29 13:03:32 +0530

    push

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('image viewer'),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/image.jpg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
